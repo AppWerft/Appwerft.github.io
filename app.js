b2fcc8da7211aa80d453d522363c15b26b17b0b2
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-01-13 17:58:55 +0100

    wip

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ function onReady() {
 };
 
 function renderItem(item,ndx) {
-	$('#accordion').append('<h3>'+item.projekt+'<div style="float:right"></div></h3><div id="slot_'+ndx+'">'+ JSON.stringify(item)+'</div>');
-	if (item.technik.indexOf('cordova')>-1) $('h3 div').append('<img src="assets/cordova.png" height="30"/>')
+	$('#accordion').append('<h3>'+item.projekt+'<div style="float:right" id="icons_'+ndx+'"></div></h3><div id="slot_'+ndx+'">'+ JSON.stringify(item)+'</div>');
+	if (item.technik.indexOf('cordova')>-1) 
+		$('h3 div').append('<img src="assets/cordova.png" height="30"/>')
 }
 
 $(document).ready(onReady);
