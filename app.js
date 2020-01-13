18619a28fd4164205b0d06f63b70d71dbc0544c2
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-01-13 17:28:57 +0100

    Init

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-$(document).ready(onReady);
-
 function onReady() {
-        $("#accordion").accordion({
-           active: 1,
-           collapsible: true
-        });
+    
 	$.getJSON('./projekte.json',function(json){
 		document.title = json.title;
 		$('h1').text(json.title);
 		
 		json.items.forEach(renderItem)
+		$("#accordion").accordion({
+           active: 1,
+           collapsible: true
+    });
 	});
 };
 
@@ -18,3 +17,7 @@ function renderItem(item) {
 		$('#accordion').append('<h3>'+item.projekt+'</h3><div>'+ JSON.stringify(item)+'</div>');
 
 }
+
+
+
+$(document).ready(onReady);
