875d04170bae9d0f01d10b3c3ddcf4a3d2e52287
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-01-13 16:41:59 +0100

    Init

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(onReady);
 function onReady() {
 	console.log('mmmm');
 	$.getJSON('./projekte.json',function(json){
-		$(body).append('<h1>'+json.title+'</h1>');
+		$('body').append('<h1>'+json.title+'</h1>');
 	});
 
 };
