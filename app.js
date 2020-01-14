a373022084636c7f2c40d3569d13f52f0507738b
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-01-14 10:49:51 +0100

    rendering content+

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function onReady() {
 	$.getJSON('./projekte.json',function(model){
 		document.title = model.title;
-		$('h1').text(model.title).prepend('<img src="'+model.image+'" width="80" />');
+		$('h1').text(model.title).prepend('<img id="avatar" src="'+model.image+'" />');
 		model.items.map(renderItem);
 		$("#accordion").accordion({collapsible: true, active: false});
 	});
