8f7d7291eef7f55e1f9f8b0cd282de5beb8b46d4
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-01-14 10:09:25 +0100

    language decoration

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 (function(){
-function onReady() {
-	$.getJSON('./projekte.json',function(model){
-		document.title = model.title;
-		$('h1').text(model.title);
-		$("#accordion").accordion();
-		model.items.map(renderItem);
+	function onReady() {
+		$.getJSON('./projekte.json',function(model){
+			document.title = model.title;
+			$('h1').text(model.title);
+			$("#accordion").accordion();
+			model.items.map(renderItem);
 		
-	});
-};
-
-
-function renderArticle(item,ndx) {
-	return '<dl><dt>Projekt</dt><dd>'+item.projekt+'</dd></dl>';
-}
+		});
+	};
+	function renderArticle(item,ndx) {
+		return '<dl><dt>Projekt</dt><dd>'+item.projekt+'</dd></dl>';
+	}
   
-function renderItem(item,ndx) {
-	console.log("renderItem " + ndx);
-	$('#accordion').append('<h3>'+item.projekt+'<div style="float:right" id="icons_'+ndx+'"></div></h3><div>' +renderArticle(item,ndx)+'</div>');
-	const langs = item.technik.split(/,\s+/);
-	['Cordova','JSP','Java','ES6','ObjectiveC','Titanium','Android','iOS','ReactNative','Javascript'].forEach(function(k){
-		if (langs.indexOf(k)>-1) $('#icons_'+ndx).append('<img src="assets/'+k.toLowerCase()+'.png" height="24"/>')
-	});
-}
-
-$(document).ready(onReady);
+	function renderItem(item,ndx) {
+		console.log("renderItem " + ndx);
+		$('#accordion').append('<h3>'+item.projekt+'<div style="float:right" id="icons_'+ndx+'"></div></h3><div>' +renderArticle(item,ndx)+'</div>');
+		const langs = item.technik.split(/,\s+/);
+		['Cordova','JSP','Java','ES6','ObjectiveC','Titanium','Android','iOS','ReactNative','Javascript'].forEach(function(k){
+			if (langs.indexOf(k)>-1) $('#icons_'+ndx).append('<img src="assets/'+k.toLowerCase()+'.png" height="24"/>')
+		});
+	}
+	$(document).ready(onReady);
 })();
