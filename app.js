f009619fbd8f216ffa764ce96ffa3b35099349cf
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-01-14 09:42:52 +0100

    language decoration

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function renderItem(item,ndx) {
 	$('#accordion').append('<h3>'+item.projekt+'<div style="float:right" id="icons_'+ndx+'"></div></h3><div id="slot_'+ndx+'">'+ JSON.stringify(item)+'</div>');
 	const langs = item.technik.split(/,\s+/);
 	console.log(langs);
-	['cordova','JSP','Java','ES6','ObjectiveC','Titanium','Android','iOS','ReactNative'].forEach(function(k){
+	['Cordova','JSP','Java','ES6','ObjectiveC','Titanium','Android','iOS','ReactNative'].forEach(function(k){
 		if (langs.indexOf(k)>-1) 
 		$('#icons_'+ndx).append('<img src="assets/'+k.toLowerCase()+'.png" height="24"/>')
 	});
