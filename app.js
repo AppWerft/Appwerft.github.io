0b84ad25ef420ede760503d593289a44816a8d44
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-01-14 13:00:42 +0100

    xp, php mysql added

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function renderItem(item,ndx) {
 	$('#accordion').append('<h3>'+item.projekt+'<div style="float:right" id="icons_'+ndx+'"></div></h3><div>' +renderArticle(item,ndx)+'</div>');
 	const langs = item.technik.split(/,\s+/);
 	['WindowsXP','PHP','mySQL','Cordova','JSP','Java','ES6','ObjectiveC','Titanium','Android','iOS','ReactNative','Javascript'].forEach(function(k){
-		if (langs.indexOf(k)>-1) $('#icons_'+ndx).append('<img src="assets/'+k.toLowerCase()+'.png" height="24"/>')
+		if (langs.indexOf(k)>-1) $('#icons_'+ndx).append('<img title="'+k+'" src="assets/'+k.toLowerCase()+'.png" height="24"/>')
 	});
 }
 
