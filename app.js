39054e3ac6040f451606a34e9b2871d37669d0e5
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-02-13 17:55:57 +0100

    projectes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function onReady() {
 		$("#accordion").accordion({collapsible: true, active: false, heightStyle: "content"});
 		$('[title!=""]').qtip({position: {my: 'top right', at: 'bottom right'},style: { classes: 'qtip-dark qtip-shadow'}});
 		
-		if (opts["expanded"]) 
+		if (opts["expanded"] && opts["expanded"]=='all') 
 			$(".ui-accordion-content").show();
 	});
 };
