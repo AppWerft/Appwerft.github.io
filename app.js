5a264cd5154b33a0203987fa3df176dbf98507d0
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-02-13 18:09:21 +0100

    projectes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function onReady() {
 			});
 			if (opts["expand"] && opts["expand"]=='selected') 
 			PIDs.forEach(function(p,i){
+				console.log('#ui-id-'+(i+1));
 				$('#ui-id-'+(i+1)).next().show();	
 			});
 		} else projects = model.items;
