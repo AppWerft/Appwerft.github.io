d7ccafe379b28bd27ac4c69f628514bdf6299d64
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-05-30 17:05:21 +0200

    init

---
+++ fangohr/app.unfaelle.js
@@ -0,0 +1,46 @@
+const Unfälle = function (heatmapLayer) {
+    this.heatmapLayer = heatmapLayer;
+    this.data = null;
+
+    $.get('./unfaelle.csv?1', function (_csv) {
+        Log('csv loaded ...')
+        this.data = $.csv.toObjects(_csv, { separator: ';', }).filter(function () {
+            return (arguments[0].ULAND == '2')
+        })
+        Log('csv loaded ...')
+        const heatdata = this.data.map(function (d) {
+            return {
+                count: d.UKATEGORIE * 50,
+                lat: parseFloat(d.YGCSWGS84.replace(',', '.')),
+                lng: parseFloat(d.XGCSWGS84.replace(',', '.'))
+            };
+        });
+        this.heatmapLayer.setData({ data: heatdata });
+        Log('csv set')
+
+    }.bind(this));
+    return this;
+};
+
+Unfälle.prototype.setFilter = function (field, enabled) {
+    console.log(field + '  ' + enabled);
+    var filtereddata= [];
+    console.log(this.data[0])
+    if (enabled) {
+        filtereddata = this.data.filter(function (d) {
+            return d[field] == '1';
+        })
+    } else {
+        filtereddata = this.data.filter(function (d) {
+            return d[field] == '0';
+        });
+    }
+    const heatdata = filtereddata.map(function (d) {
+        return {
+            count: d.UKATEGORIE * 50,
+            lat: parseFloat(d.YGCSWGS84.replace(',', '.')),
+            lng: parseFloat(d.XGCSWGS84.replace(',', '.'))
+        };
+    });
+    this.heatmapLayer.setData({ data: heatdata });
+}
--- fangohr/index.js
+++ fangohr/index.js
@@ -2,36 +2,36 @@ var start = new Date().getTime();
 
 function Log(text) {
 	var now = new Date().getTime();
-	console.log((now-start)+ ' '+text);
-	start=now;
+	console.log((now - start) + ' ' + text);
+	start = now;
 }
 
 Log(start);
 window.onload = function () {
 	this.Log('onload');
-	$('.drawer').drawer();
+	this.Drawer = $('.drawer').drawer();
 	const Map = new L.Map('unfallkarte', {
-		center: new L.LatLng(53.5562788,9.985348),
+		center: new L.LatLng(53.5562788, 9.985348),
 		zoom: 14,
 		layers: []
 	});
 	L.tileLayer.wms(
 		'https://geodienste.hamburg.de/HH_WMS_DOP?', {
 		service: 'WMS',
-		version :'1.3.',
+		version: '1.3.',
 		request: 'GetMap',
 		format: 'image/jpeg',
 		transparent: 'true',
 		layers: 1,
 		width: 512,
 		height: 512,
-		opacity:0.9,
+		opacity: 0.9,
 		crs: L.CRS.EPSG25832
 	}).addTo(Map);
-	
+
 	var heatmapLayer = new HeatmapOverlay({
 		"radius": 8,
-		max : 10,
+		max: 10,
 		"maxOpacity": 1,
 		"scaleRadius": false,
 		"useLocalExtrema": true,
@@ -40,27 +40,21 @@ window.onload = function () {
 		lngField: 'lng',
 	});
 	Map.addLayer(heatmapLayer);
-
+	const UnfallLayer = new Unfälle(heatmapLayer);
 	Log('addL')
-	$.get('./unfaelle.csv?1', function (_csv) {
-		Log('csv loaded ...')
-		const csv = $.csv.toObjects(_csv, { separator: ';', }).filter(function () {
-			return (arguments[0].ULAND == '2')
-		})
-		Log('toObject ...')
-		var data = csv.map(function (d) {
-			return {
-				count: d.UKATEGORIE*50,
-				lat: parseFloat(d.YGCSWGS84.replace(',', '.')),
-				lng: parseFloat(d.XGCSWGS84.replace(',', '.'))
-			};
-		});
-		Log('mapping')
+	setTimeout(function () {
+		$('.drawer').drawer('open');
+	}, 3000);
+	const beteiligung = { istRad: 'Fahrrad', IstPKW: 'Personenkraftwagen', IstFuss: 'Fußgänger' ,IstKrad:'Kraftrad',IstGkfz:'Güterkraftwagen',IstSonstig: 'Sonstiges'};
+	this.Object.keys(beteiligung).forEach(function (item) {
+		$('#beteiligung').append('<li class="drawer-menu-item"><label class="switch"><input checked="1" type="checkbox" name="' + item + '"><span class="slider round"></span></label>' + beteiligung[item] + '</li>')
+	}),
+	$('#beteiligung input').on('change',function(){
+		const self = $(this);	
+		
+		UnfallLayer.setFilter(self.attr('name'),self.is(':checked'))
 
-		
-	    // Filter depending of drawer
-		heatmapLayer.setData({ data: data });
-		Log('csv set')
-		
+		console.log(self);
 	});
+
 };
