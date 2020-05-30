1c563840aa02524528b6c93feca6bf608bec8029
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-05-30 15:45:33 +0200

    init

--- fangohr/index.js
+++ fangohr/index.js
@@ -1,10 +1,19 @@
+var start = new Date().getTime();
+
+function Log(text) {
+	var now = new Date().getTime();
+	console.log((now-start)+ ' '+text);
+	start=now;
+}
+
+Log(start);
 window.onload = function () {
+	this.Log('onload');
 	$('.drawer').drawer();
 	const Map = new L.Map('unfallkarte', {
 		center: new L.LatLng(53.5562788,9.985348),
 		zoom: 14,
 		layers: []
-
 	});
 	L.tileLayer.wms(
 		'https://geodienste.hamburg.de/HH_WMS_DOP?', {
@@ -16,32 +25,42 @@ window.onload = function () {
 		layers: 1,
 		width: 512,
 		height: 512,
+		opacity:0.9,
 		crs: L.CRS.EPSG25832
 	}).addTo(Map);
+	
+	var heatmapLayer = new HeatmapOverlay({
+		"radius": 8,
+		max : 10,
+		"maxOpacity": 1,
+		"scaleRadius": false,
+		"useLocalExtrema": true,
+		valueField: 'count',
+		latField: 'lat',
+		lngField: 'lng',
+	});
+	Map.addLayer(heatmapLayer);
 
+	Log('addL')
 	$.get('./unfaelle.csv?1', function (_csv) {
+		Log('csv loaded ...')
 		const csv = $.csv.toObjects(_csv, { separator: ';', }).filter(function () {
 			return (arguments[0].ULAND == '2')
 		})
+		Log('toObject ...')
 		var data = csv.map(function (d) {
 			return {
-				count: 10,
+				count: d.UKATEGORIE*50,
 				lat: parseFloat(d.YGCSWGS84.replace(',', '.')),
 				lng: parseFloat(d.XGCSWGS84.replace(',', '.'))
 			};
 		});
+		Log('mapping')
 
-		var heatmapLayer = new HeatmapOverlay({
-			"radius": 5,
-			"maxOpacity": 0.7,
-			"scaleRadius": false,
-			"useLocalExtrema": true,
-			valueField: 'count',
-			latField: 'lat',
-			lngField: 'lng',
-		});
-
+		
+	    // Filter depending of drawer
 		heatmapLayer.setData({ data: data });
-		Map.addLayer(heatmapLayer);
+		Log('csv set')
+		
 	});
 };
