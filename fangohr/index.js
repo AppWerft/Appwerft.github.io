c74a312feb1d9c94baa16a2242436e462caacff9
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-06-06 10:59:33 +0200

    fullscreen added

--- fangohr/index.js
+++ fangohr/index.js
@@ -27,7 +27,7 @@ const onLoad = function (address) {
 		cursor: true,
 		layers: []
 	});
-	Map.toggleFullscreen()
+	Map.isFullscreen()  || Map.toggleFullscreen()
 	Map.addControl(new L.Control.Fullscreen({
 		title: {
 			'false': 'View Fullscreen',
