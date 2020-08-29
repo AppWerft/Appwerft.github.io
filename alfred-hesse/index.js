eab8449b1af6fc576d0b0053b61e1e2ad52946c0
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-08-29 11:59:37 +0200

    hesse added

--- alfred-hesse/index.js
+++ alfred-hesse/index.js
@@ -75,7 +75,25 @@ const onLoad = function (address) {
 	hammertime.on('swipe', function (ev) {
 		$('.drawer').drawer('close');
 	});
-	
+	L.Control.Watermark = L.Control.extend({
+		onAdd: function (map) {
+			var img = L.DomUtil.create('img');
+			img.src = 'ah.png';
+			img.style.width = '360px';
+
+			return img;
+		},
+
+		onRemove: function (map) {
+			// Nothing to do here
+		}
+	});
+
+	L.control.watermark = function (opts) {
+		return new L.Control.Watermark(opts);
+	}
+
+	L.control.watermark({ position: 'bottomright' }).addTo(Map);
 
 };
 
