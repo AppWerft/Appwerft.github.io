ea4095f1cb9efdbcfb8f621b0241ef37dc59525e
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-08-29 11:49:33 +0200

    hesse added

--- alfred-hesse/index.js
+++ alfred-hesse/index.js
@@ -51,123 +51,31 @@ const onLoad = function (address) {
 		Map.closePopup(popup);
 	});
 
-	Map.on('click', function (ev) {
-		var latlng = Map.mouseEventToLatLng(ev.originalEvent);
-
-		//const msg = ;
-		const event = UnfallLayer.getNearestUnfall(latlng.lat, latlng.lng);
-		popup = L.popup()
-			.setLatLng(L.latLng(event.YGCSWGS84, event.XGCSWGS84))
-			.setContent(renderEvent(event));
-
-		Map.openPopup(popup);
-	});
-/*
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-		Map.on('zoomstart', function () {
-			UnfallLayer.hide();
-		})
-		Map.on('zoomend', function () {
-			UnfallLayer.show();
-		})
-	}
-*/
+	
 	const kategorien = { '1': "Aquarelle", '2': "Kreidezeichnungen", '3': "Kohlezeichnungen" };
 	Object.keys(kategorien).forEach(function (id) {
 		$('#kategorie').append('<li class="drawer-menu-item"><label class="switch"><input checked="1" type="checkbox" name="' + id + '"><span class="slider round"></span></label><legend>' + kategorien[id] + '</legend></li>')
 	});
-
-
-	
-	/*
-	setTimeout(function () {
-		var wochentage = UnfallLayer.getTotal('UWOCHENTAG');
-		var pie = new d3pie('wochentage', {
-			size: {
-				canvasHeight: 240,
-				canvasWidth: 240
-			},
-			data: {
-				content: 'Mo Di Mi Do Fr Sa So'.split(' ').map(function (tag, i) {
-					return {
-						label: tag,
-						value: wochentage[i + 1]
-					}
-				})
-			},
-			callbacks: {
-				onMouseoverSegment: function (info) {
-					
-				},
-				onMouseoutSegment: function (info) {
-				}
-			}
+	function onWerkeLoad(data) {
+		data.forEach(function(w){
+			var marker = L.circle(w.gps.split(','),{
+				color:'red',
+				fillColor: '#f03',
+				fillOpacity: 0.5,	
+				radius: 40000
+			}).addTo(Map).bindPopup('<p><b>'+w.name+'</b></p><img width="300" src="tx_userahwerke/'+w.img+'" />');	
+    		
 		});
-	}, 200);
-	setTimeout(function () {
-		var monate = UnfallLayer.getTotal('UMONAT');
-
-		var pie = new d3pie('monate', {
-			size: {
-				canvasHeight: 240,
-				canvasWidth: 240
-			},
-			data: {
-				content: 'Jan Feb März Apr Mai Juni Juli Aug Sept Okt Nov Dez'.split(' ').map(function (tag, i) {
-					return {
-						label: tag,
-						value: monate[i + 1]
-					}
-				})
-			},
-			callbacks: {
-				onMouseoverSegment: function (info) {
-					console.log('mouse in', info);
-				},
-				onMouseoutSegment: function (info) {
-				}
-			}
-		});
-	}, 200);
-    */
-	$('#beteiligung input').on('change', function () {
-		const self = $(this);
-		UnfallLayer.setIst(self.attr('name'), self.is(':checked'))
-	});
-	$('#kategorie input').on('change', function () {
-		const self = $(this);
-		UnfallLayer.setKategorie(self.attr('name'), self.is(':checked'))
-	});
-	L.Control.Watermark = L.Control.extend({
-		onAdd: function (map) {
-			var img = L.DomUtil.create('img');
-			img.src = 'https://upload.wikimedia.org/wikipedia/commons/5/52/Hamburg-logo.svg';
-			img.style.width = '240px';
-
-			return img;
-		},
-
-		onRemove: function (map) {
-			// Nothing to do here
-		}
-	});
-
-	L.control.watermark = function (opts) {
-		return new L.Control.Watermark(opts);
 	}
 
-	L.control.watermark({ position: 'bottomright' }).addTo(Map);
+	$.getJSON('werke.json',onWerkeLoad);
+	
 	var hammertime = new Hammer(document.getElementsByClassName('drawer-menu')[0], {});
 	hammertime.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
 	hammertime.on('swipe', function (ev) {
 		$('.drawer').drawer('close');
 	});
-	Map.addControl(new L.Control.Fullscreen({
-		title: {
-			'false': 'View Fullscreen',
-			'true': 'Exit Fullscreen'
-		}
-	}));
+	
 
 };
 
